71e4b9bc9fb3469bac805f2942b635c86c8b83a0
github username <github email address>
2021-02-02 21:02:01 -0500

    auto-correct

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,9 +4,11 @@ import SearchBar from '../components/SearchBar';
 
 
 const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+
     return(
         <View>
-            <SearchBar/>
+            <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)}/>
             <Text>Search Screen</Text>
         </View>
     );
