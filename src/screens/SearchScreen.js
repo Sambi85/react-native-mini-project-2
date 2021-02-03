f0616773133fc582d82ecea8c202a5bf3ad9e228
github username <github email address>
2021-02-02 21:20:08 -0500

    test

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
-
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
 
