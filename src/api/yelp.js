121ba2665556ce8fcc79007b979a94019178972b
github username <github email address>
2021-02-03 22:04:35 -0500

    yelp-search-api

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -6,5 +6,3 @@ export default axios.create({
         Authorization: 'Bearer wX-6R8xox2CbLCmNAxB-CVcDVmdbDt2Ij0kCnh6tjX4rUayldDbIUslqH0mnnAw4CeEu_SAtxhuwp-E6C7m2jrsEtFrdYu-PAdGYM2ABwszziiTQB9dnzj1C3b4YYHYx'
     }
 });
-
-yelp.get('/search')
