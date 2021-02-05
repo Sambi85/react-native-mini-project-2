61c4b3106a69a44a67d7d8d2d6f6b2e6178e630e
github username <github email address>
2021-02-05 18:30:57 -0500

    result-list-comp

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
@@ -19,6 +19,9 @@ const SearchScreen = () => {
             <Text>Search Screen</Text>
             { errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ResultsList/>
+            <ResultsList/>
+            <ResultsList/>
         </View>
     );
 };
