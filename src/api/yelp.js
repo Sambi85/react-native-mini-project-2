8a1a5985e3cc609446731628b7ec067bf8cfe762
github username <github email address>
2021-02-05 18:18:21 -0500

    use-results

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,7 +4,7 @@ export default axios.create({
 
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        g
+        
         Authorization: 'Bearer wX-6R8xox2CbLCmNAxB-CVcDVmdbDt2Ij0kCnh6tjX4rUayldDbIUslqH0mnnAw4CeEu_SAtxhuwp-E6C7m2jrsEtFrdYu-PAdGYM2ABwszziiTQB9dnzj1C3b4YYHYx'
     }
 });
