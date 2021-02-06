0aa96d7ac8bfb8f6a10fb53513bd2004fb9d7419
github username <github email address>
2021-02-05 22:51:29 -0500

    clean-up

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -7,7 +7,7 @@ const ResultsList = ({ title, results }) => {
         <FlatList 
           horizontal
           data={results}
-          keyExtractor={(result) => {result.id}}
+          keyExtractor={(result) => result.id}
           renderItem={ ({ item }) => {
             return <Text>{item.name}</Text>
           }}
