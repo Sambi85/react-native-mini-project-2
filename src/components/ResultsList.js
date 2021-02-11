7b605fa4e6ceb24f39abcf36ea3c149801331f38
github username <github email address>
2021-02-11 12:50:00 -0500

    if-statement

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,23 +4,30 @@ import { withNavigation } from 'react-navigation';
 import ResultsDetail from './ResultsDetail'; 
 
 const ResultsList = ({ title, results, navigation }) => {
-    return <View style={styles.container}>
-        <Text style={styles.title}>{title}</Text>
-        <FlatList 
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          data={results}
-          keyExtractor={(result) => result.id}
-          renderItem={ ({ item }) => {
-            return (
-                <TouchableOpacity onPress={ () => {navigation.navigate('ResultsShow', { id: item.id })} }> 
-                   <ResultsDetail result={item}/>
-                </TouchableOpacity>
+
+    if(!results.length) { 
+        return null; 
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{title}</Text>
+            <FlatList 
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={results}
+                keyExtractor={(result) => result.id}
+                renderItem={ ({ item }) => {
+                
+                return (
+                    <TouchableOpacity onPress={ () => {navigation.navigate('ResultsShow', { id: item.id })} }> 
+                        <ResultsDetail result={item}/>
+                    </TouchableOpacity>
                 )
-          }}
-        />
-    </View>
-};
+                }}
+            />
+        </View>
+    )};
 
 const styles = StyleSheet.create({
     title: {
